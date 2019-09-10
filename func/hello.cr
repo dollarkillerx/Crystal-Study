10293688b83d1e1ffa524cb0dfe37e69d414fbf7
Dollarkillerx <dollarkiller@vip.qq.com>
2019-09-10 23:12:00 +0800

    add

---
+++ func/hello.cr
@@ -0,0 +1,15 @@
+def hello
+  puts "hello world"
+end
+
+hello
+
+def sayHello(name="我是你爸爸")
+    puts "hello #{name}"
+end
+
+sayHello
+
+puts "===================="
+
+sayHello("你好")
---
+++ func/hello.rb
@@ -0,0 +1,15 @@
+def hello
+  puts "你好"
+end
+
+def sayHello(name="123")
+  puts "Hello #{name}"
+end
+
+hello
+
+sayHello
+
+puts "============+"
+
+sayHello("我是你爸爸")
